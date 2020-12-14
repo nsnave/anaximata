9858c2aa19761651f271dc339124b9c265f6c56e
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2020-12-13 23:50:08 -0500

    added options for transition diagrams and graphs

--- automata/options.js
+++ automata/options.js
@@ -1,3 +1,21 @@
+//initializes options form
+let type = document.getElementById("automata");
+type.style.maxHeight = type.scrollHeight + "px";
+
+//handles changing the type (graph, transition digrapm, etc.)
+document.getElementById("type").addEventListener("change", function () {
+  let s = document.getElementById("type");
+  let opt = s.options;
+
+  for (let i = 0; i < opt.length; i++) {
+    document.getElementById(opt[i].value).style.maxHeight = null;
+  }
+
+  let val = document.getElementById(s.value);
+  val.style.maxHeight = val.scrollHeight + "px";
+});
+
+//handles changing color of selected tool button
 let mode_btns = document.getElementsByClassName("mode_button");
 
 for (let i = 0; i < mode_btns.length; i++) {
