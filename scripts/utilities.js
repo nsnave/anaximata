2f4638d70bd5bd81fdca080cf9ac28f6be8e018b
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2021-02-15 18:33:01 -0500

    finished scrollbars
    hopefully I'll figure out how to add transitions back to the "type change" later but no transitions will work for now

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -139,3 +139,34 @@ function lineFromSlopeAndPoint(slope, point) {
       : point.y - slope * point.x;
   return temp;
 }
+
+// Calculates scroll bar width
+// Source: https://stackoverflow.com/a/55278118/11039508
+class TempScrollBox {
+  constructor() {
+    this.scrollBarWidth = 0;
+
+    this.measureScrollbarWidth();
+  }
+
+  measureScrollbarWidth() {
+    // Add temporary box to wrapper
+    let scrollbox = document.createElement("div");
+
+    // Make box scrollable
+    scrollbox.style.overflow = "scroll";
+
+    // Append box to document
+    document.body.appendChild(scrollbox);
+
+    // Measure inner width of box
+    this.scrollBarWidth = scrollbox.offsetWidth - scrollbox.clientWidth;
+
+    // Remove box
+    document.body.removeChild(scrollbox);
+  }
+
+  get width() {
+    return this.scrollBarWidth;
+  }
+}
